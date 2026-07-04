d3f2d90e6e60914301e0523bc6d8785996e01fc0
ayusha1223 <ayushathapa425@gmail.com>
2026-07-04 11:39:32 +0545

    Admin controller added

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,40 @@
 const User = require("../models/User");
 const Destination = require("../models/Destination");
 const Itinerary = require("../models/Itinerary");
+/*
+|--------------------------------------------------------------------------
+| DELETE DESTINATION
+|--------------------------------------------------------------------------
+*/
 
+exports.deleteDestination = async (req, res) => {
+
+  try {
+
+    const destination =
+      await Destination.findByIdAndDelete(req.params.id);
+
+    if (!destination) {
+
+      return res.status(404).json({
+        message: "Destination not found",
+      });
+
+    }
+
+    res.json({
+      message: "Destination deleted successfully",
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message,
+    });
+
+  }
+
+};
 exports.getDashboardStats = async (req, res) => {
   try {
 
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
 const protect = require("../middleware/authMiddleware");
 const admin = require("../middleware/adminMiddleware");
-
 const {
   getDashboardStats,
+  deleteDestination,
 } = require("../controllers/adminController");
 
 router.get(
@@ -13,6 +12,12 @@ router.get(
   protect,
   admin,
   getDashboardStats
+);
+router.delete(
+  "/destinations/:id",
+  protect,
+  admin,
+  deleteDestination
 );
 
 module.exports = router;
